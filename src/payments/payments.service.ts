e0a9ded2489d5fcfb43dc0dc6b3f2d1405790a29
aldry <albuquerquealdry@gmail.com>
2021-11-03 13:36:17 -0300

    "Reformulação do model"

--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -1,13 +1,12 @@
-import { Body, Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { create } from 'domain';
-import { Model, Models } from 'mongoose';
-import { CreatePaymentDto } from './dto/create-payment.dto';
-import { UpdatePaymentDto } from './dto/update-payment.dto';
-import { PaymentsDocument, Payments } from './entities/payment.entity';
+import { Body, Injectable } from '@nestjs/common'
+import { InjectModel } from '@nestjs/mongoose'
+import { create } from 'domain'
+import { Model, Models } from 'mongoose'
+import { CreatePaymentDto } from './dto/create-payment.dto'
+import { UpdatePaymentDto } from './dto/update-payment.dto'
+import { PaymentsDocument, Payments } from './entities/payment.entity'
 import { PaymentsModule } from './payments.module';
-
-const IdSearch = require('../payments/scripts/ms-cadastro-getway')
+const IdSearch = require('./scripts/ms-cadastro-getway')
 
 @Injectable()
 export class PaymentsService {
@@ -15,32 +14,43 @@ constructor(@InjectModel(Payments.name) private PaymentsModel: Model<PaymentsDoc
   create(createPaymentDto: CreatePaymentDto) {
     console.log(createPaymentDto)
   }
-  findAll() {
+
+  findAll(){
     return this.PaymentsModel.find()
   }
-  findOne(id: string) {    
+
+  findOne(id:string){    
     var find = this.PaymentsModel.findById(id)
-    var idResponse=  String(find['_conditions']['_id'])
-    const parseUser=(name, email, cpf, birthday,)=>{
-      const Payment =  new this.PaymentsModel({name:name, email:email, 
-      cpf:cpf,birthday:birthday, pay:true, delivery: false})   
+    var idResponse = String(find['_conditions']['_id'])
+    const parseUser =(name, email, cpf, birthday,)=>{
+      const Payment =  new this.PaymentsModel({
+        name:name,
+        email:email,
+        cpf:cpf,
+        birthday:birthday,
+        pay:true,
+        delivery:false})   
       return Payment.save()
     }
+
     async function IdResult(ids){
-      var getResponse =  await IdSearch(ids)
-      var respResponse = await(getResponse)
-      var name =respResponse['name']
-      var email =respResponse['email']
-      var cpf = respResponse ['cpf']
-      var birthday= respResponse ['birthday']
-      parseUser(name, email, cpf, birthday)
+      const getResponse = await IdSearch(ids)
+      const respResponse = await(getResponse)
+      const name = respResponse['name']
+      const email = respResponse['email']
+      const cpf = respResponse ['cpf']
+      const birthday = respResponse ['birthday']
+      parseUser(name,email,cpf,birthday)
     }
+
     IdResult(id)
   }
-  update(id: string, updatePaymentDto: UpdatePaymentDto) {
-    return `This action updates a #${id} payment`;
+
+  update(id: string, updatePaymentDto: UpdatePaymentDto){
+    return `This action updates a #${id} payment`
   }
-  remove(id: string) {
-    return `This action removes a #${id} payment`;
+
+  remove(id: string){
+    return `This action removes a #${id} payment`
   }
 }
